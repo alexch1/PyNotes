cbe9c5b09ee70646a5759f4a958234d59007b88c
Ji Chi <chiji.zju@gmail.com>
2016-05-26 17:32:46 +0800

    Update 1-GetPrimeNumbers.py

--- 1-GetPrimeNumbers.py
+++ 1-GetPrimeNumbers.py
@@ -1,8 +1,7 @@
 # -*- coding:utf-8 -*- 
- 
+
 def get_threshold():
 # get the length and generate an random integer with the specific legth.
- 
     choice = int(input('\nChoose the type (1 or 2):\n1. set a specific threshold number\n2. Randomly generated a threshold\n'))
     if choice == 1:
         number = int(input('\nPlease input your threshold number:\n'))        
@@ -13,18 +12,17 @@ def get_threshold():
         print ("\nThe threshold value is randomly set to be:", number)
     return number
  
- 
 def print_prime_numbers(threshold):
     import math
-    for num in range(1,threshold+1):
+    for num in range(2,threshold+1):
         flag = True
         for divnum in range(1,int(math.sqrt(num))+1):
-            if (num % divnum == 0) and (divnum != num) and (divnum != 1):
+            if (not num % divnum) and (divnum - num) and (divnum - 1):
                 flag = False
                 break
         if flag:
             print (num)
- 
+
 def main():
     print("""
     -------------------------------------------------------
@@ -41,7 +39,7 @@ def main():
     print ("\nPrime numbers below ", threshold, "are shown below:")
     print_prime_numbers(threshold)
     print ("\n")
- 
+
 # if and only if you run the code from this file.
 if __name__ == '__main__':
     main()
